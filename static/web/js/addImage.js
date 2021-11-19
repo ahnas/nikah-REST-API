c585cb8586ef0db8493e27e05359f40d65894426
mubashir-cvr <ahnas>
2021-11-19 15:29:47 +0530

    cosmatic changese

--- static/web/js/addImage.js
+++ static/web/js/addImage.js
@@ -6,14 +6,12 @@ $(document).ready(function () {
     beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'Token ' + localStorage.getItem('token')); },
     success: function (response) {
       if (response['userProperties'] == false) {
-
         if (pageURL != "http://127.0.0.1:8000/profiler") {
           window.location.href = "http://127.0.0.1:8000/profiler"
         }
       }
 
       if (response['user'] == false) {
-
         if (pageURL != "http://127.0.0.1:8000/profilerB") {
           window.location.href = "http://127.0.0.1:8000/profilerB"
         }
